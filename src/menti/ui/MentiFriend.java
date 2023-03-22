57c56f0fc69dcd54deb7af10fb0d5b87da71f350
kamyarajesh <kam.r2576@gmail.com>
2023-03-22 13:22:49 -0400

    passowrd

--- src/menti/ui/MentiFriend.java
+++ src/menti/ui/MentiFriend.java
@@ -1,11 +1,36 @@
 package menti.ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
 
-public class MentiFriend {
-    public static void main(String[] args){
-        System.out.println("hello");
-    }
+public class MentiFriend extends JFrame{
 
     private JPanel mainPanel;
+    private JPasswordField passwordInput;
+    private JButton enterPasswordButton;
+
+    public MentiFriend() {
+        enterPasswordButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(enterPasswordButton, "you entered: " + passwordInput.getText());
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        MentiFriend m = new MentiFriend();
+        m.setContentPane(m.mainPanel);
+        m.setTitle("Hii");
+        m.setSize(300,400);
+        m.setVisible(true);
+        m.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+
+
+
 }
