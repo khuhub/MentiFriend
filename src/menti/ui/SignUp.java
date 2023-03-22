376863dbbd4a38c09e64ae46ead1dd118ab3faa2
kamyarajesh <kam.r2576@gmail.com>
2023-03-22 15:29:35 -0400

    sign up

---
+++ src/menti/ui/SignUp.java
@@ -0,0 +1,10 @@
+package menti.ui;
+
+import javax.swing.*;
+
+public class SignUp {
+    private JPanel panel1;
+    private JTextField userNameInput;
+    private JLabel userNameLabel;
+    private JTextField passwordCreate;
+}
